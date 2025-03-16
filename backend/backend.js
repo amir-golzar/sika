ae60319b0faff9e7fa9595aa20d0eb2f0b8ca3bf
amir-golzar <goliamir1384@gmail.com>
2025-03-16 06:07:33 +0000

    d

--- backend/backend.js
+++ backend/backend.js
@@ -43,7 +43,7 @@ app.post("/sida", (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ message: "eroreeeeeeeeeeeeeee", status: "noky" });
+      res.status(500).json({ message: "erorr", status: "noky" });
     });
 });
 app.get("/sida", async (req, res) => {
